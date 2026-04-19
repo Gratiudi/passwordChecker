8d0641097ac46f146731c3d80bcf32f70ad8e777
Gratiudi <tinbiteyonas97@gmail.com>
2026-04-19 16:03:29 -0700

    upper and lower case check

--- checker.py
+++ checker.py
@@ -4,8 +4,12 @@ def check_password_strength(password):
     strength_score = 0
     feedback = []
 
-    # Length Check
     if len(password) >= 12:
         strength_score += 1
     else:
         feedback.append("- Password should be at least 12 characters long.")
+
+    if re.search(r"[a-z]", password) and re.search(r"[A-Z]", password):
+        strength_score += 1
+    else:
+        feedback.append("- Include both uppercase and lowercase letters.")
